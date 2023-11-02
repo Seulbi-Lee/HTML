9c7e613329be6836511eb8ce80fd01b3df123764
Seulbi-Lee <drizzle8926@gmail.com>
2023-11-02 15:17:43 -0700

    change folder

---
+++ 00_project_main/js/js1.js
@@ -0,0 +1,13 @@
+let Object = {
+  email : 'ddd@gamial.com',
+  name : 'ddd'
+}
+
+class User {
+  constructor(email, name){
+    email : email;
+    name : name;
+  }
+}
+
+let a = new User('asd@gmail.com','asd')
